7a415f668ec7e4d7768335eb73dfb43412321de1
Klaudia Przespolewska <klaudia1000@onet.eu>
2016-12-10 16:50:28 +0100

    boksy

---
+++ js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/*console.log(document);*/
+
+/*function sprawdzam() {
+    alert("ALERT!");
+}*/
+
+function wyswietlInfo() {
+   /* alert("ALERT!");*/
+
+    if (document.getElementById("wszystkie-zgody").checked==true) 
+    {
+        document.getElementById("zgoda-marketingowa-1").checked=true;
+        document.getElementById("zgoda-marketingowa-2").checked=true;
+        document.getElementById("zgoda-marketingowa-1").disabled=true;
+        document.getElementById("zgoda-marketingowa-2").disabled=true;
+    } 
+    else {
+        document.getElementById("zgoda-marketingowa-1").checked=false;
+        document.getElementById("zgoda-marketingowa-2").checked=false;
+        document.getElementById("zgoda-marketingowa-1").disabled=false;
+        document.getElementById("zgoda-marketingowa-2").disabled=false;
+        
+    }
+}
+ 
+document.getElementById("wszystkie-zgody").addEventListener('click', wyswietlInfo)
